13080835b34a72b3de69886bb360e0a700a13648
wmadwand <wmadwand@gmail.com>
2019-02-16 14:57:47 +0300

    POI could be seeing through the walls with its permanent face towards the player

---
+++ Assets/Scripts/TargetUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class TargetUI : MonoBehaviour
+{
+	public RectTransform labelTr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		labelTr.LookAt(FindObjectOfType<RigidbodyFirstPersonController>().transform, Vector3.up);
+
+	}
+}
