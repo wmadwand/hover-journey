ce545213cf30423ba34c97091cc9e7369fe469a5
wmadwand <wmadwand@gmail.com>
2019-02-18 11:00:16 +0300

    EnemyAttack class. Missile prefab

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+	[SerializeField] private GameObject _missile;
+	[SerializeField] private Transform _shotSpawn;
+	[SerializeField] private float _shotWait;
+	[SerializeField] private float _delay;
+
+	private void OnEnable()
+	{
+		InvokeRepeating("Fire", Random.Range(1, _delay), Random.Range(1.5f, _shotWait));
+	}
+
+	private void OnDisable()
+	{
+		CancelInvoke("Fire");
+	}
+
+	void Fire()
+	{
+		Instantiate(_missile, _shotSpawn.position, _shotSpawn.rotation);		
+	}
+}
