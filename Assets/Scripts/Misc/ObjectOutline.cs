db15e2487bf3715cd98d15fd1b8e29b6f158ff94
wmadwand <wmadwand@gmail.com>
2019-02-21 15:51:24 +0300

    ObjectOutline class

---
+++ Assets/Scripts/Misc/ObjectOutline.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ObjectOutline : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	private Material _material;
+
+	private void Awake()
+	{
+		_material = GetComponent<MeshRenderer>().material;
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		_material.SetFloat("_OutlineVal", .1f);
+
+		Debug.Log("Enter");
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		_material.SetFloat("_OutlineVal", 0);
+
+		Debug.Log("Exit");
+	}
+}
