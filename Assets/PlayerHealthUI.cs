b47c1585d2cd6d9011daaef8793526c7e9a7a73b
wmadwand <wmadwand@gmail.com>
2019-02-25 11:34:02 +0300

    Display player health

---
+++ Assets/PlayerHealthUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthUI : MonoBehaviour
+{
+	private Text _text;
+
+	private void Awake()
+	{
+		_text = GetComponentInChildren<Text>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		_text.text = $"Health: { Game.Instance.Player.GetComponent<PlayerHealth>().Value}";
+	}
+}
